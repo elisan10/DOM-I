c83aed346aae74766d9e97d97eea0ab067f27716
Eli <elize.san10@gmail.com>
2021-01-11 18:54:54 -0600

    have new selectors created and most of the HTML updated

--- js/index.js
+++ js/index.js
@@ -40,3 +40,98 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//NEW SELECTORS
+
+
+//body
+const header = document.querySelector('header')
+const banner = document.querySelector('.cta')
+const mainContent = document.querySelector('.main-content')
+const contact = document.querySelector('.contact')
+const footer = document.querySelector('footer')
+
+
+//header
+const anchors = header.querySelector('nav a')
+const logoImage = header.querySelector('#logo-img')
+
+//a tags
+const servicesNav = anchors
+const productNav = servicesNav.nextElementSibling
+const visionNav = productNav.nextElementSibling
+const featuresNav = visionNav.nextElementSibling
+const aboutNav = featuresNav.nextElementSibling
+const contactNav = aboutNav.nextElementSibling 
+
+
+//banner
+const bannerHeader = banner.querySelector('h1')
+const bannerButton = banner.querySelector('button')
+const bannerImage = banner.querySelector('#cta-img')
+
+//mainContent
+const topMainContent = mainContent.querySelector('.top-content')
+const mainContentImage = mainContent.querySelector('#middle-img')
+const bottomMainContent = mainContent.querySelector('.bottom-content')
+
+const topHeaderOne = topMainContent.querySelector('h4')
+const topHeaderTwo = topHeaderOne.nextElementSibling
+const topParagraphOne = topMainContent.querySelector('p')
+const topParagraphTwo = topParagraphOne.nextElementSibling
+console.log(topHeaderOne, topHeaderTwo)
+
+const bottomHeaderOne = bottomMainContent.querySelector('h4')
+const bottomHeaderTwo = bottomHeaderOne.nextElementSibling
+const bottomParagraphOne = bottomMainContent.querySelector('p')
+const bottomParagraphTwo = bottomParagraphOne.nextElementSibling
+// const bottomHeaderThree = bottomHeaderTwo.nextElementSibling
+// const bottomParagraphThree = bottomParagraphTwo.nextElementSibling
+
+//contact
+const contactHeader = contact.querySelector('h4')
+const paragraphOne = contact.querySelector('p')
+const paragraphTwo = paragraphOne.nextElementSibling
+const paragraphThree = paragraphTwo.nextElementSibling
+
+//footer
+const footerParagraph = footer.querySelector('p')
+
+
+//UPDATED HTML
+
+servicesNav.textContent = 'Services'
+productNav.textContent = 'Product'
+visionNav.textContent = 'Vision'
+featuresNav.textContent = 'Features'
+aboutNav.textContent = 'About'
+contactNav.textContent = 'Contact'
+
+logoImage.src = 'img/logo.png'
+
+
+bannerHeader.textContent = 'DOM Is Awesome' 
+bannerButton.textContent = 'Get Started'
+bannerImage.src = 'img/header-img.png' 
+
+
+topHeaderOne.textContent = 'Features'
+topParagraphOne.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+topHeaderTwo.textContent = 'About'
+// topParagraphTwo.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+mainContentImage.src = 'img/mid-page-accent.jpg'
+
+bottomHeaderOne.textContent = 'Services'
+bottomHeaderTwo.textContent = 'Product'
+
+
+
+contactHeader.textContent = 'Contact'
+paragraphOne.textContent = '123 Way 456 Street Somewhere, USA'
+paragraphTwo.textContent = '1 (888) 888-8888'
+paragraphThree.textContent = 'sales@greatidea.io'
+
+footerParagraph.textContent = 'Copyright Great Idea! 2018'
+//NEW CONTENT
